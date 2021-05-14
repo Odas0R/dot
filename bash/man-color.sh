044fcacf20461f3609466eeef321f10b2dd41514
odas0R <g.rosado@campus.fct.unl.pt>
2021-05-14 17:23:33 +0100

    removed coc-vim, went to vscode 😂

--- bash/man-color.sh
+++ bash/man-color.sh
@@ -4,3 +4,5 @@ export LESS_TERMCAP_us=$'\e[01;32m'
 export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_so=$'\e[45;93m'
 export LESS_TERMCAP_se=$'\e[0m'
+
+export LS_COLORS="di=1;36"
--- bash/mark.sh
+++ bash/mark.sh
@@ -1,6 +1,1 @@
-# Script Paths
-export MARKPATH=$HOME/.marks
 
-jump() {
-  [ -h "$MARKPATH/$1" ] && cd -P "$MARKPATH/$1" ||  echo "mark not found"
-}
