7da2829fd6db25020e8f14212bc66b9cf54c7f2f
odas0R <g.rosado@campus.fct.unl.pt>
2021-07-27 03:29:01 +0100

    automated from gitsync... 🎉

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -34,3 +34,12 @@ alias gp="git push"
 alias gb="git branch -a -l -v"
 alias gf="git fetch origin"
 alias gl="git log --pretty=format:'%C(blue)%h%C(red)%d %C(white)%s - %C(cyan)%cn,%C(green)%cr'"
+
+# really can't understand why this wont work as a script.
+jump() {
+  if [ -h "$MARKPATH/$1" ]; then
+    cd -P "${MARKPATH}/${1}" && (gitsync &)
+  else
+    echo "mark not found"
+  fi
+}
