dcb4257939a56db5d7ccd2156a163fae4c9b4939
odas0R <g.rosado@campus.fct.unl.pt>
2021-08-12 20:21:21 +0100

    automated from gitsync... 🎉

--- bash/path.sh
+++ bash/path.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
 # Custom Path Variables
-export MARKPATH=$HOME/.marks
-export SNIPPETS=$HOME/snippets
-export SCRIPTS=$HOME/.local/bin/scripts
+export MARKPATH="$HOME/.marks"
+export SNIPPETS="$HOME/snippets"
+export SCRIPTS="$HOME/.local/bin/scripts"
 
 # NVM (Node Version Manager)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 export GOPATH="$HOME/go"
 
 # Global Path
-export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin/scripts"
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$SCRIPTS"
