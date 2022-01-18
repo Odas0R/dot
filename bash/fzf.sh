99ba3a6b0846ec06f83c7af4068f41a5ec580d79
odas0R <g.rosado@campus.fct.unl.pt>
2022-01-18 11:04:55 +0000

    feat(fzf): added tokyo theme to fzf

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -8,7 +8,14 @@ FD_OPTIONS="--follow --exclude .git --exclude node_modules"
 FZF_BINDS="--bind 'f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-/:toggle-all'"
 FZF_STYLES="--height=90% --layout=reverse --preview-window right:60%"
 
-export FZF_DEFAULT_OPTS="--no-mouse ${FZF_STYLES} ${FZF_BINDS}"
+FZF_TOKYO_THEME='
+	--color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
+	--color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff
+	--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff 
+	--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
+
+export FZF_DEFAULT_OPTS="--no-mouse ${FZF_STYLES} ${FZF_BINDS} ${FZF_TOKYO_THEME}"
+
 export FZF_DEFAULT_COMMAND="fd --type f --type l ${FD_OPTIONS}"
 export FZF_CTRL_T_COMMAND="fd ${FD_OPTIONS}"
 export FZF_ALT_C_COMMAND="fd --type d ${FD_OPTIONS}"
