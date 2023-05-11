539fe3091349db8623a3126c9d0e0c3d208c4b77
odas0r <g.rosado@campus.fct.unl.pt>
2023-05-11 13:16:15 +0100

    fixed checkbox plugin

--- nvim/lua/odas0r/checkbox.lua
+++ nvim/lua/odas0r/checkbox.lua
@@ -1,66 +1,86 @@
 local api = vim.api
 local keymap = vim.keymap.set
 
+local checked_character = "x"
+
 local states = {
-  unchecked = " ",
-  checked = "x",
+  unchecked = "%[ %]",
+  checked = "%[" .. checked_character .. "]",
 }
 
-local function toggle()
-  local current_line = api.nvim_get_current_line()
-  local is_checked = current_line:match("%[" .. states.checked .. "%]")
-
-  if is_checked then
-    current_line = current_line:gsub("%[" .. states.checked .. "%]", "%[" .. states.unchecked .. "%]")
-  else
-    current_line = current_line:gsub("%[" .. states.unchecked .. "%]", "%[" .. states.checked .. "%]")
-  end
-
-  api.nvim_set_current_line(current_line)
+local line_contains_an_unchecked_checkbox = function(line)
+  return line:find(states.unchecked)
 end
 
--- Utils
+local checkbox = {
+  check = function(line)
+    return line:gsub(states.unchecked, states.checked)
+  end,
+  uncheck = function(line)
+    return line:gsub(states.checked, states.unchecked)
+  end,
+}
 
-local function get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
-  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+local toggle = function()
+  local bufnr = vim.api.nvim_buf_get_number(0)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local start_line = cursor[1] - 1
+  local current_line = vim.api.nvim_buf_get_lines(bufnr, start_line, start_line + 1, false)[1] or ""
+
+  local new_line = ""
+  if line_contains_an_unchecked_checkbox(current_line) then
+    new_line = checkbox.check(current_line)
   else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+    new_line = checkbox.uncheck(current_line)
   end
-  return lines
+
+  vim.api.nvim_buf_set_lines(bufnr, start_line, start_line + 1, false, { new_line })
+  vim.api.nvim_win_set_cursor(0, cursor)
 end
 
-local function toggle_many()
-  local s_start = vim.fn.getpos("'<")[2] - 1
-  local s_end = vim.fn.getpos("'>")[2]
+local toggle_many = function()
+  local bufnr = vim.api.nvim_buf_get_number(0)
+  local cursor = vim.api.nvim_win_get_cursor(0)
 
-  local lines = get_visual_selection()
-  for k, current_line in pairs(lines) do
-    local is_checked = current_line:match("%[" .. states.checked .. "%]")
+  local pos_cursor = vim.fn.getpos(".")
+  local pos = vim.fn.getpos("v")
 
-    if is_checked then
-      current_line = current_line:gsub("%[" .. states.checked .. "%]", "%[" .. states.unchecked .. "%]")
+  local start_line = pos[2]
+  local end_line = pos_cursor[2]
+
+  if end_line < start_line then
+    local t_end_line = start_line
+    local t_start_line = end_line
+    start_line = t_start_line
+    end_line = t_end_line
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, end_line, false)
+  local new_lines = {}
+
+  for _, line in ipairs(lines) do
+    if line_contains_an_unchecked_checkbox(line) then
+      local new_line = checkbox.check(line)
+      table.insert(new_lines, new_line)
     else
-      current_line = current_line:gsub("%[" .. states.unchecked .. "%]", "%[" .. states.checked .. "%]")
+      local new_line = checkbox.uncheck(line)
+      table.insert(new_lines, new_line)
     end
-
-    lines[k] = current_line
   end
 
-  api.nvim_buf_set_lines(0, s_start, s_end, 0, lines)
+  vim.api.nvim_buf_set_lines(bufnr, start_line - 1, end_line, false, new_lines)
+  vim.api.nvim_win_set_cursor(0, cursor)
 end
+
+-- [x] Hello everybody
+-- [x] Hellooooooooooo
+-- [ ] Oi
 
 -- Checkbox
 keymap("n", "<leader>x", function()
-  vim.print("toggle")
-  -- toggle()
-end, { silent = false })
+  toggle()
+end, { silent = true })
 
 keymap("v", "<leader>x", function()
   toggle_many()
-end, { silent = true })
+end, { silent = false })
