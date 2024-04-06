523048ff6d404b7a015a5c0dd0f48cee3680eb92
odas0r <guilherme.odas0r@gmail.com>
2024-04-06 21:19:51 +0100

    first commit on this branch

--- bash/editor.sh
+++ bash/editor.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-_hasDuckyKeyboard() {
-  test -n "$(lsusb -v 2>/dev/null | grep -E '(^Bus|Keyboard)' | grep -w 'Varmilo Keyboard')"
-}
-
-# remap capslock for whole system to ESC (X only)
-if [ -n "${DISPLAY}" ]; then
-  # https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
-  if _hasDuckyKeyboard; then
-    setxkbmap -layout pt -model macbook79
-  else
-    setxkbmap -layout pt -model pc102
-  fi
-
-  # Default Options
-  setxkbmap -option caps:escape
-fi
+# this is not needed under wsl2
+# _hasDuckyKeyboard() {
+#   test -n "$(lsusb -v 2>/dev/null | grep -E '(^Bus|Keyboard)' | grep -w 'Varmilo Keyboard')"
+# }
+#
+# # remap capslock for whole system to ESC (X only)
+# if [ -n "${DISPLAY}" ]; then
+#   # https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
+#   if _hasDuckyKeyboard; then
+#     setxkbmap -layout pt -model macbook79
+#   else
+#     setxkbmap -layout pt -model pc103
+#   fi
+#
+#   # Default Options
+#   setxkbmap -option caps:escape
+# fi
 
 export HRULEWIDTH=73
 
