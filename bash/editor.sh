3597b31bfebd78ed040d4055070628464ac769c9
odas0r <guilherme.odas0r@gmail.com>
2024-04-07 12:37:04 +0100

    fix browser

--- bash/editor.sh
+++ bash/editor.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
 
-# this is not needed under wsl2
-# _hasDuckyKeyboard() {
-#   test -n "$(lsusb -v 2>/dev/null | grep -E '(^Bus|Keyboard)' | grep -w 'Varmilo Keyboard')"
-# }
-#
-# # remap capslock for whole system to ESC (X only)
-# if [ -n "${DISPLAY}" ]; then
-#   # https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
-#   if _hasDuckyKeyboard; then
-#     setxkbmap -layout pt -model macbook79
-#   else
-#     setxkbmap -layout pt -model pc103
-#   fi
-#
-#   # Default Options
-#   setxkbmap -option caps:escape
-# fi
-
 export HRULEWIDTH=73
 
 # Define the editor
 export VISUAL="nvimr"
 export EDITOR="$VISUAL"
 
-ln -s "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" ~/.local/bin/chrome
+# execute this to symlink the chrome program 
+# ln -sf "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" ~/.local/bin/chrome
+#
 export BROWSER=~/.local/bin/chrome
